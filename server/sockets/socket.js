896585d30f56099583fa5043518a31c59f95ffd9
Hernan Arena <hernan.arena@gmail.com>
2019-02-05 23:16:47 -0300

    Chat con interfaz de usuario

--- server/sockets/socket.js
+++ server/sockets/socket.js
@@ -14,20 +14,23 @@ io.on('connection', (client) => {
     client.join(data.sala);
     usuarios.agregarPersona(client.id, data.nombre, data.sala);
     client.broadcast.to(data.sala).emit('listaPersona',usuarios.getPersonasPorSala(data.sala));
+    client.broadcast.to(data.sala).emit('crearMensaje',crearMensaje('administrador',`${data.nombre} unió`));
     callback(usuarios.getPersonasPorSala(data.sala));
   });
 
-  client.on('crearMensaje',(data)=>{
+  client.on('crearMensaje',(data, callback)=>{
     let persona = usuarios.getPersona(client.id);
     let mensaje = crearMensaje(persona.nombre, data.mensaje);
     client.broadcast.to(persona.sala).emit('crearMensaje',mensaje);
+
+    callback( mensaje )
   })
 
 
 
   client.on('disconnect',()=>{
     let personaBorrada = usuarios.borrarPersonas(client.id);
-    client.broadcast.to(personaBorrada.sala).emit('crearMensaje',crearMensaje('administrador',`${personaBorrada.nombre}salió`));
+    client.broadcast.to(personaBorrada.sala).emit('crearMensaje',crearMensaje('administrador',`${personaBorrada.nombre} salió`));
     client.broadcast.to(personaBorrada.sala).emit('listaPersona',usuarios.getPersonasPorSala(personaBorrada.sala));
   });
 
